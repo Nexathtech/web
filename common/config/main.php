3574ab976a567b61aaed660e720f7e41daf80ff3
footniko <Footniko@gmail.com>
2017-09-21 20:34:55 +0300

    Test email sending

--- common/config/main.php
+++ common/config/main.php
@@ -84,7 +84,7 @@ $config = [
             'targets' => [
                 [
                     'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning', 'info'],
+                    'levels' => ['error', 'warning'],
                 ],
             ],
         ],
