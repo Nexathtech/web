514968895e3e13710e234b71b7d1f100b7b1b336
footniko <Footniko@gmail.com>
2017-09-21 19:05:48 +0300

    Test email sending

--- api/controllers/SiteController.php
+++ api/controllers/SiteController.php
@@ -14,9 +14,18 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
+        $to = 'Footniko@gmail.com';
+        $subject = 'Simple Mail';
+        $message = 'This is a text';
+        $headers = 'From: webmaster@meetkodi.com' . "\r\n" .
+            'Reply-To: webmaster@meetkodi.com' . "\r\n" .
+            'X-Mailer: PHP/' . phpversion();
+
+        mail($to, $subject, $message, $headers);
+
         return Yii::$app->mailer->compose()
             ->setFrom('admin@meetkodi.com')
-            ->setTo('Footniko@gmail.com')
+            ->setTo($to)
             ->setSubject('This is a test message')
             ->setTextBody('This is a plain text message content')
             ->send();
