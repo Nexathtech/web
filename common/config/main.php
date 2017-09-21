f54e06ab3353263c48cb994b66198fa6a02d691b
footniko <Footniko@gmail.com>
2017-09-21 19:36:29 +0300

    Test email sending

--- common/config/main.php
+++ common/config/main.php
@@ -80,7 +80,7 @@ $config = [
 
         // Logging
         'log' => [
-            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'traceLevel' => YII_ENV_LOCAL ? 3 : 0,
             'targets' => [
                 [
                     'class' => 'yii\log\FileTarget',
