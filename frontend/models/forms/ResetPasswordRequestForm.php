1f586964711d3fe9ca53c08a1474e2fffa8afe25
footniko <Footniko@gmail.com>
2017-09-21 22:02:29 +0300

    Password reset functionality

--- frontend/models/forms/ResetPasswordRequestForm.php
+++ frontend/models/forms/ResetPasswordRequestForm.php
@@ -51,9 +51,9 @@ class ResetPasswordRequestForm extends Model
         if (!$user) {
             return false;
         }
-        return true;
         
         $token = Yii::$app->security->generateToken($user->id, TokenType::PASSWORD_RESET);
+        return true;
         $resetTokenUrl = str_replace('api.', '', Url::to(["/auth/password-reset/$token"], true));
 
         return Yii::$app->mailer->compose('password-reset-token', [
