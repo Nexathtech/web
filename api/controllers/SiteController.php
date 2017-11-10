45a3a429c22c4ccde2ed53fd0e572d6416f1bb02
footniko <Footniko@gmail.com>
2017-11-10 19:39:11 +0200

    Mailing test

--- api/controllers/SiteController.php
+++ api/controllers/SiteController.php
@@ -14,13 +14,6 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        return Yii::$app->mailer->compose()
-            ->setFrom('admin@meetkodi.com')
-            ->setTo('Footniko@gmail.com')
-            ->setSubject('This is a test message')
-            ->setTextBody('This is a plain text message content')
-            ->send();
-
-        //return Yii::t('app', 'It works!');
+        return Yii::t('app', 'It works!');
     }
 }
