9cdbdfbd773d811409ca1b2bb3655c0f5efa92c2
footniko <Footniko@gmail.com>
2018-02-06 15:49:18 +0200

    #21 Update email template

--- common/mail/layouts/html.php
+++ common/mail/layouts/html.php
@@ -1,9 +1,12 @@
 <?php
 use yii\helpers\Html;
+use yii\helpers\Url;
 
 /* @var $this \yii\web\View view component instance */
 /* @var $message \yii\mail\MessageInterface the message being composed */
 /* @var $content string main view render result */
+
+$homeUrl = str_replace('api.', '', Url::to(['/'], true));
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -15,7 +18,18 @@ use yii\helpers\Html;
 </head>
 <body>
     <?php $this->beginBody() ?>
-    <?= $content ?>
+    <div style="padding: 20px;background: #ffce46;">
+        <a href="<?= $homeUrl; ?>" class="logo" style="display: block;width: 100px;height: 48px;margin: 0 auto;background: url(<?= $homeUrl; ?>/styles/img/logo-black.png) no-repeat;background-size: 100%;"></a>
+    </div>
+
+    <div class="content" style="padding: 50px;font-size: 16px;color: #45433d;">
+        <div class="content-inner">
+            <?= $content ?>
+        </div>
+    </div>
+
+    <?= $this->render('includes/_social_links', ['homeUrl' => $homeUrl]) ?>
+    <div class="footer" style="width: 100%;height: 110px;background: url(<?= $homeUrl ?>/styles/img/footer-waves2.png) repeat-x;"></div>
     <?php $this->endBody() ?>
 </body>
 </html>
--- common/mail/welcome.php
+++ common/mail/welcome.php
@@ -1,15 +1,24 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\Url;
 
 /* @var $this yii\web\View */
 /* @var $user \kodi\common\models\user\User */
 /* @var $confirmationUrl string Absolute url */
 
+$homeUrl = str_replace('api.', '', Url::to(['/'], true));
 ?>
 
+<div class="thank-preset" style="margin-top: 50px;text-align: center;">
+    <img src="<?= $homeUrl ?>/styles/img/thanks-to-be.png" style="max-width: 100%;">
+</div>
 <div>
     <p>Hello <?= Html::encode($user->profile->name); ?>,</p>
     <p>Thanks for your registration! In order to activate your account, please follow this link:</p>
-    <p><strong><?= Html::a(Html::encode($confirmationUrl), $confirmationUrl); ?></strong></p>
+    <p>
+        <?= Html::a('Activate my account', $confirmationUrl, [
+            'style' => 'display: inline-block; padding: 10px 20px; background: #05bc45; color: #fff;'
+        ]); ?>
+    </p>
 </div>
