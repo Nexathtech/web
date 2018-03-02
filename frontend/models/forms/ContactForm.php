e600e117cb818549d9ba825202fdec449185192c
footniko <Footniko@gmail.com>
2018-03-02 16:28:49 +0200

    #27

--- frontend/models/forms/ContactForm.php
+++ frontend/models/forms/ContactForm.php
@@ -36,9 +36,10 @@ class ContactForm extends Model
     public function sendEmail()
     {
         $subject = Yii::t('frontend', 'Kodi Team') . ': ' . Yii::t('frontend', 'Contact form');
+        $sender = ArrayHelper::getValue(Yii::$app->params, 'adminEmail');
 
         return Yii::$app->mailer->compose()
-            ->setFrom(ArrayHelper::getValue(Yii::$app->params, 'adminEmail'))
+            ->setFrom([$sender => Yii::t('frontend', 'Kodi Contact Form')])
             ->setReplyTo($this->email)
             ->setTo(Yii::$app->settings->get('system_email_sender'))
             ->setSubject($subject)
