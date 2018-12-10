d4674d4d2d44fad42ff785e36af5591d8cf48e47
footniko <Footniko@gmail.com>
2018-12-10 20:06:02 +0200

    Add login_type to sign-in method

--- api/models/auth/SignIn.php
+++ api/models/auth/SignIn.php
@@ -4,6 +4,7 @@ namespace kodi\api\models\auth;
 
 use kodi\common\enums\DeviceType;
 use kodi\common\enums\user\Status;
+use kodi\common\enums\user\Type;
 use kodi\common\models\user\User;
 use Yii;
 use yii\base\Model;
@@ -40,6 +41,12 @@ class SignIn extends Model
      * @var string $type Device's type (Mobile or Kiosk)
      */
     public $type;
+
+    /**
+     * @var string $login_type Simple or Brand.
+     * Just need to show an error if a Simple user tries to sign in as a Brand
+     */
+    public $login_type;
 
     /**
      * @var IdentityInterface $_identity
@@ -79,10 +86,13 @@ class SignIn extends Model
                     $this->addError('password', Yii::t('api', 'User with such credentials is not found.'));
                     return;
                 }
-                if ($this->_identity && $this->_identity->status === Status::INACTIVE) {
+                if ($this->login_type === Type::BRAND && $this->_identity->type !== Type::BRAND) {
+                    $this->addError('email', Yii::t('api', 'Mismatch account type. Please contact us to become a Brand.'));
+                }
+                if ($this->_identity->status === Status::INACTIVE) {
                     $this->addError('email', Yii::t('api', 'Your account is not activated.'));
                 }
-                if ($this->_identity && $this->_identity->status === Status::SUSPENDED) {
+                if ($this->_identity->status === Status::SUSPENDED) {
                     $this->addError('email', Yii::t('api', 'Your account is suspended.'));
                 }
             }],
